076c70b559213def7ca1cd6abee5be4c529045f7
THIRUVASAGAM22 <124457012+THIRUVASAGAM22@users.noreply.github.com>
2023-12-25 15:27:05 +0530

    Buffer overflow code

---
+++ Buffer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+char buffer[10];
+
+printf("Enter your string : ");
+scanf("%s",buffer);
+printf("Your string : %s\n",buffer);
+
+printf("Enter your long string :");
+scanf("%s",buffer);
+printf("Your long string : %s \n",buffer);
+
+return 0;
+
+}
